e2d5ea25da834706884d56a0ce020fe67d6f3b7e
garganshul108 <sci.agarg@gmail.com>
2019-08-02 01:53:19 +0530

    refactoring resaturants

--- bookmytable/src/components/restaurants.jsx
+++ bookmytable/src/components/restaurants.jsx
@@ -1,40 +1,110 @@
 import React, { Component } from "react";
 import { getRestaurants } from "../api/dump/restaurant";
-import TableRow from "./table/tableRow";
 import displayColumns from "./restaurantTableConfig.json";
+import { captialize } from "../util/util";
+
+import "./css/restaurants.css";
 
 class Restaurants extends Component {
   state = {
-    data: []
+    restaurants: []
   };
 
   // called once the component is initialized
   // place for all the API calls etc
   componentDidMount() {
-    let data = getRestaurants();
-    this.setState({ data });
+    let restaurants = getRestaurants();
+    this.setState({ restaurants });
+  }
+
+  renderRestaurant(restaurant) {
+    function refactorName(name) {
+      return captialize(name);
+    }
+
+    function refactorCuisines(cuisines) {
+      return cuisines.split(",");
+    }
+
+    console.log(restaurant.id);
+    var {
+      id,
+      name,
+      highlights: tags,
+      average_cost_for_two: avCost,
+      cuisines,
+      currency
+    } = restaurant;
+
+    cuisines = refactorCuisines(cuisines);
+    name = refactorName(name);
+
+    function renderTags() {
+      return (
+        <React.Fragment>
+          {tags.map(tag => (
+            <span className="badge badge-info">{tag}</span>
+          ))}
+        </React.Fragment>
+      );
+    }
+
+    function renderCuisines() {
+      return (
+        <React.Fragment>
+          {cuisines.map(cuisine => (
+            <span className="badge badge-danger">{cuisine}</span>
+          ))}
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <div className="row restaurantDisplay" key={id}>
+        <div className="col-2">
+          <h6 style={{ textAlign: "center" }}>Image</h6>
+          <p style={{ textAlign: "center" }}>
+            here the image will come once the path is set
+          </p>
+        </div>
+
+        <div className="col-7 restaurantDesc">
+          <h4>{name}</h4>
+
+          <p>
+            Av cost: {currency}
+            {avCost}
+          </p>
+          <div>{renderCuisines()}</div>
+          <div>{renderTags()}</div>
+        </div>
+
+        <div className="col-3">
+          <h6 style={{ textAlign: "center" }}>Extra</h6>
+          <p style={{ textAlign: "center" }}>Booking buttons etc</p>
+        </div>
+      </div>
+    );
   }
 
   render() {
-    const { data } = this.state;
-    const count = data.length;
+    const { restaurants } = this.state;
+    const count = restaurants.length;
+
+    console.log(restaurants);
     return (
       <React.Fragment>
-        <h1>Restaurants: {count}</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              {displayColumns.map(column => {
-                return <th key={column._id}>{column.title}</th>;
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(item => (
-              <TableRow key={item._id} content={item} />
-            ))}
-          </tbody>
-        </table>
+        <div className="row" style={{ marginBottom: "150px" }}>
+          <div className="col-2">
+            <p>Filter</p>
+          </div>
+          <div className="col">
+            <h3>Restaurants: {count}</h3>
+            <div className="container-fluid restaurantCatalog">
+              {restaurants.map(restaurant => this.renderRestaurant(restaurant))}
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
