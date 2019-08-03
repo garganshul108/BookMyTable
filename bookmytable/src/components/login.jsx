f670597c432a10f232e33bbee24fa30ce5ec4b30
garganshul108 <sci.agarg@gmail.com>
2019-08-03 14:59:07 +0530

    login page initialed

--- bookmytable/src/components/login.jsx
+++ bookmytable/src/components/login.jsx
@@ -1,8 +1,101 @@
 import React, { Component } from "react";
 
+import "./css/login.css";
+
 class Login extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <React.Fragment>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2">Left</div>
+            <div className="col loginDiv">
+              <small class="form-text text-muted">Already a User?</small>
+              <h4>Login to continue</h4>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                  />
+                  <small id="emailHelp" className="form-text text-muted">
+                    We'll never share your email with anyone else.
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div className="col signupDiv">
+              <small class="form-text text-muted">New User?</small>
+              <h4>Welcome to SignUp</h4>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                  />
+                  <small id="emailHelp" className="form-text text-muted">
+                    We'll never share your email with anyone else.
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div className="col-2">Right</div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
