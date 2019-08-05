72652545673c0faf8473ee9f7ec61fa7b5567ec2
Sumit Tomar <sumittomar023@gmail.com>
2019-08-06 02:30:55 +0530

    ToCheck

--- BackEnd/API/main.py
+++ BackEnd/API/main.py
@@ -68,7 +68,6 @@ def get_restaurants():
         resp = jsonify(rows)
         resp.status_code = 200
         return resp
-        # return "good"
     except Exception as e:
         print(e)
     finally:
