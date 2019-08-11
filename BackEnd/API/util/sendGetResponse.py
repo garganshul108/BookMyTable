ae11776c0def1d7c432476f461532c12869046cf
Sumit Tomar <sumittomar023@gmail.com>
2019-08-12 00:05:41 +0530

    SmChange

--- BackEnd/API/util/sendGetResponse.py
+++ BackEnd/API/util/sendGetResponse.py
@@ -7,5 +7,5 @@ def send_get_response(data,resp):
 
     res=jsonify(data)
     res.status_code = 200
-    res.headers[Access-Control-Allow-Origin]='*'
+    res.headers['Access-Control-Allow-Origin']='*'
     return res
