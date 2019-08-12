65caa9e99976b03d09e53970f3ea08e325137259
Sumit Tomar <sumittomar023@gmail.com>
2019-08-13 01:42:16 +0530

    LocalityGetWorking

--- BackEnd/API/Location/getAllCities.py
+++ BackEnd/API/Location/getAllCities.py
@@ -5,12 +5,18 @@ from flask import jsonify
 from flask import flash, request
 from util.sendGetResponse import send_get_response
 
-@app.route('/cities')
+@app.route('/localities')
 def get_cities():
+    city=request.args.get('city')
+    if city==None:
+        resp=jsonify("City Required")
+        resp.status_code=400
+        return resp
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT DISTINCT city FROM Location")
+        # cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT locality FROM Location WHERE city=%s",city)
         rows = cursor.fetchall()
         return send_get_response(rows,"No city found")
     except Exception as e:
