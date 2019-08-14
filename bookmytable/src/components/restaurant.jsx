691de8e52575004af2e86d1bbb414d8a2bd6965e
garganshul108 <sci.agarg@gmail.com>
2019-08-14 18:10:26 +0530

    added gallery

--- bookmytable/src/components/restaurant.jsx
+++ bookmytable/src/components/restaurant.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { Switch, Route, Redirect } from "react-router-dom";
 import RestaurantBookingWindow from "./subComponents/restaurantBookingWindow";
 import NotFound from "./notFound";
@@ -7,6 +6,17 @@ import { Link } from "react-router-dom";
 import ReviewInputForm from "./subComponents/reviewInputForm";
 import { getRestaurantById } from "../services/restaurantServices";
 import "./css/restaurant.css";
+import PhotoGallery from "./subComponents/photoGallery";
+
+let photo = [
+  "https://b.zmtcdn.com/data/pictures/chains/5/18895645/24279bed659c9c07ea57444d841a305c.jpg?crop=3738%3A3738%3B764%2C0&fit=around%7C200%3A200",
+  "https://b.zmtcdn.com/data/pictures/chains/1/491/d9cdb557070f37c148b81aacd239593c.jpg?crop=3744%3A3744%3B1392%2C0&fit=around%7C200%3A200",
+  "https://b.zmtcdn.com/data/pictures/chains/1/491/5807c28869fee0e18235f22fca139e24.jpg?crop=277%3A277%3B0%2C0&fit=around%7C200%3A200",
+  "https://b.zmtcdn.com/data/pictures/chains/1/491/4698634d17bd06561f4f97573865fd9e.jpg?crop=332%3A332%3B93%2C0&fit=around%7C200%3A200",
+  "https://b.zmtcdn.com/data/pictures/chains/1/491/a8ed0bf5795596bc6e69f1347d7b6b8c.jpg?crop=301%3A301%3B61%2C0&fit=around%7C200%3A200"
+];
+
+photo = [...photo, ...photo, ...photo, ...photo];
 
 const wideDisplayString =
   "?fit=around%7C920%3A200%20&crop=920%3A200%3B%2A%2C%2A";
@@ -325,7 +335,7 @@ class Restaurant extends Component {
 
                   <Route
                     path="/restaurant/:id"
-                    render={props => <NotFound {...props} />}
+                    render={props => <PhotoGallery photos={photo} {...props} />}
                   />
                   <Route
                     path="/restaurant/:id"
