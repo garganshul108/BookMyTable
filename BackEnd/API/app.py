34f2add32bb07de22b6cb490635ded2fd9e2f84d
Sumit Tomar <sumittomar023@gmail.com>
2019-08-16 02:38:30 +0530

    CORSS

--- BackEnd/API/app.py
+++ BackEnd/API/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__, static_url_path='/static')
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SECRET_KEY']='thisissecret'
+
 
