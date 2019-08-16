f96c60f06994d48ccaa77e90231b7d00ceda6b20
garganshul108 <sci.agarg@gmail.com>
2019-08-16 13:42:14 +0530

    --

--- BackEnd/install.sh
+++ BackEnd/install.sh
@@ -1,4 +1,2 @@
 python3 -m venv venv && . venv/bin/activate
-pip install Flask && pip install flask-mysql && pip install flask_jwt-extended passlib && pip install flask_restplus
-
-pip install flask_cors
+pip install Flask && pip install flask-mysql && pip install flask_jwt-extended passlib && pip install flask_restplus && pip install flask_cors
