1d10689ea1a497324c1bef1ae8e4641ea4d927ce
Sumit Tomar <sumittomar023@gmail.com>
2019-08-17 23:22:50 +0530

    UpdatedDatabaseName

--- BackEnd/API/db_config.py
+++ BackEnd/API/db_config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'tsumit23'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'to23898it'
-app.config['MYSQL_DATABASE_DB'] = 'BookMyTable'
+app.config['MYSQL_DATABASE_DB'] = 'bookmytable'
 app.config['MYSQL_DATABASE_HOST'] = 'bookmytable1.cpd6yq0bvtmu.us-east-2.rds.amazonaws.com'
 mysql.init_app(app)
