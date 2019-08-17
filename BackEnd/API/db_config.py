7f476edf8fdd8488abaf9466d08211fe25cfbd2a
Sumit Tomar <sumittomar023@gmail.com>
2019-08-17 22:13:56 +0530

    DatabaseChangeForDeployment

--- BackEnd/API/db_config.py
+++ BackEnd/API/db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'sumit'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'qwerty'
+app.config['MYSQL_DATABASE_USER'] = 'tsumit23'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'To23898iT'
 app.config['MYSQL_DATABASE_DB'] = 'BookMyTable'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'bookmytable1.cpd6yq0bvtmu.us-east-2.rds.amazonaws.com'
 mysql.init_app(app)
