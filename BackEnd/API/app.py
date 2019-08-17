8f53299d721a2a451821b77b91ff5e1391b15508
garganshul108 <sci.agarg@gmail.com>
2019-08-17 12:38:43 +0530

    --

--- BackEnd/API/app.py
+++ BackEnd/API/app.py
@@ -4,11 +4,5 @@ app = Flask(__name__, static_url_path='/static')
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 UPLOAD_FOLDER = 'static/photos'
 app.config['SECRET_KEY']='thisissecret'
-<<<<<<< HEAD
-app.config['token_expire_time']=160
-
-
-=======
 app.config['token_expire_time']=60
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
->>>>>>> 3bc334d7aa75e9da614359cacf25b7a65fcd02fb
