66af893e72a703a269eef8c4df1b029e8b8eb469
Sumit Tomar <sumittomar023@gmail.com>
2019-08-17 23:18:45 +0530

    ChangePassword

--- BackEnd/API/db_config.py
+++ BackEnd/API/db_config.py
@@ -5,7 +5,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'tsumit23'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'To23898iT'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'to23898it'
 app.config['MYSQL_DATABASE_DB'] = 'BookMyTable'
 app.config['MYSQL_DATABASE_HOST'] = 'bookmytable1.cpd6yq0bvtmu.us-east-2.rds.amazonaws.com'
 mysql.init_app(app)
