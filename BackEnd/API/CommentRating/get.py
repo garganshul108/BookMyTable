9d996ceb3b3ccb29505a98ab8e58defc1a734234
Sumit Tomar <sumittomar023@gmail.com>
2019-08-17 18:33:26 +0530

    SomeChangesInReviewGet

--- BackEnd/API/CommentRating/get.py
+++ BackEnd/API/CommentRating/get.py
@@ -7,26 +7,44 @@ from util.sendGetResponse import send_get_response
 
 def append_photos(cursor,review):
     cursor.execute("SELECT * from Photo where review_id=%s",review['id'])
-    review['photos']=cursor.fetchall()
+    review['photos']=[]
+    for photo in cursor.fetchall():
+        review['photos'].append(photo['url'])
+    
+
+def append_restaurant_details(cursor,review):
+    sql="""SELECT Restaurant.id,Restaurant.name,Restaurant.thumb,Location.locality,Location.city 
+                    from Restaurant JOIN Location 
+                    ON  Restaurant.location_id=Location.id where Restaurant.id=%s"""
+    cursor.execute(sql,review['restaurant_id'])
+    review['restaurant']=cursor.fetchall()[0]
+    del review['restaurant_id']
+
+def append_user_details(cursor,review):
+    cursor.execute("SELECT id,name,city from User where id=%s",review['user_id'])
+    review['user']=cursor.fetchall()[0]
+    del review['user_id']
+
 
 @app.route('/api/reviews')
-def get_reviews(resId=None):
+def get_reviews(resId=None,userId=None):
     _restaurant_id=request.args.get('restaurantId',default="%",type=int)
     _user_id=request.args.get('userId',default="%",type=int)
     if resId != None:
-        _restaurant_id=resId
+        _restaurant_id='%'
+    if userId!=None:
+        _user_id='%'
     try:
         conn=mysql.connect()
         cursor=conn.cursor(pymysql.cursors.DictCursor)
-        print(_user_id)
-        # cursor.execute("SELECT * from Review where restaurant_id LIKE %s AND user_id LIKE %s ",(_restaurant_id,_user_id))
-        cursor.execute("SELECT * from Review where restaurant_id LIKE %s",(_restaurant_id))
+        cursor.execute("SELECT * from Review where restaurant_id LIKE %s AND user_id LIKE %s",(_restaurant_id,_user_id))
 
         reviews=cursor.fetchall()
         print(reviews)
         for review in reviews:
             append_photos(cursor,review)
-        
+            append_restaurant_details(cursor,review)
+            append_user_details(cursor,review)
         return send_get_response(reviews,"No Review Found ")
   
     except Exception as e:
--- BackEnd/API/User/util/convertUser.py
+++ BackEnd/API/User/util/convertUser.py
@@ -6,8 +6,10 @@ from util.sendGetResponse import send_get_response
 from LoginSignUp.util.required import token_required
 from User.BeenThere.get import get_beenthere
 from User.Bookmark.get import get_bookmark
+from CommentRating.get import get_reviews
 
 def convert_user(cursor,rows):
     for row in rows:
         get_beenthere(row=row)
         get_bookmark(row=row)
+        row['reviews']=get_reviews(userId=row['id']).json
